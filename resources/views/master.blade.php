7fc12e2c89c1e6fac9a84bdd1311ac1760000c8a
Ripley White <pearlcodes@outlook.com>
2024-02-15 17:56:09 +0000

    uwu

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -110,11 +110,9 @@
             >
                 @yield('permanent-fixed-footer')
 
-                @if ($GLOBALS['cfg']['osu']['is_development_deploy'])
-                    <div class="development-deploy-footer">
-                        This is a development instance of the <a href="https://osu.ppy.sh" class="development-deploy-footer__link">osu! website</a>. Please do not login with your osu! credentials.
-                    </div>
-                @endif
+                <div class="development-deploy-footer">
+                    this aint osu lol
+                </div>
             </div>
         </div>
 
